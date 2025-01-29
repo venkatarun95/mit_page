23cfdfd2fa4aad4ad9eea5798b415993c919013d
Venkat Arun <venkatarun95@gmail.com>
2025-01-29 14:47:13 -0600

    Added png files to permissions.sh

--- permissions.sh
+++ permissions.sh
@@ -1,3 +1,3 @@
 shopt -s globstar
-chmod 644 **/*.html **/*.js **/*.pdf **/*.jpg **/*.gif **/*.css
+chmod 644 **/*.html **/*.js **/*.pdf **/*.jpg **/*.gif **/*.css **/*.png
 chmod 755 s25 f24 sys_perf_analysis assets assets/img
