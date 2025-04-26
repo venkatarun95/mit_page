0fa868e122b7b9bd2fdcf95af01c88b33cc36958
Venkat Arun <venkatarun95@gmail.com>
2025-04-26 18:48:03 -0500

    Updated permissions to include assets/js

--- permissions.sh
+++ permissions.sh
@@ -1,3 +1,3 @@
 shopt -s globstar
-chmod 644 **/*.html **/*.js **/*.pdf **/*.jpg **/*.gif **/*.css **/*.png
-chmod 755 s25 s25/papers f24 sys_perf_analysis assets assets/img
+chmod 644 **/*.html **/*.js **/*.pdf **/*.jpg **/*.gif **/*.css **/*.png assets/js/site.js
+chmod 755 s25 s25/papers f24 sys_perf_analysis assets assets/img assets/js
