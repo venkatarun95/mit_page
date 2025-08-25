028447142305116e56f68289e51ca8ff6a78745f
Venkat Arun <venkat@utexas.edu>
2025-08-25 16:44:39 -0500

    Clarified course policies and office hours

--- permissions.sh
+++ permissions.sh
@@ -1,3 +1,3 @@
 shopt -s globstar
 chmod 644 **/*.html **/*.js **/*.pdf **/*.jpg **/*.gif **/*.css **/*.png assets/js/site.js
-chmod 755 s25 s25/papers f24 sys_perf_analysis assets assets/img assets/js
+chmod 755 f25 f25/assets f25/assets/img f25/assets/slides s25 s25/papers f24 sys_perf_analysis assets assets/img assets/js
