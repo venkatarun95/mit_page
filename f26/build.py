4427d50b8d0b0c45e0575af5dd716cd5e3f3e629
Venkat Arun <venkat@utexas.edu>
2026-09-22 17:35:39 -0500

    Fixed issues and assignment description

--- f26/build.py
+++ f26/build.py
@@ -33,11 +33,11 @@ def prep_schedule():
         "Transport layer: alternate designs", "Congestion control", "Queuing disciplines and switches",
         "Encryption 1", "Encryption 2", "Web security", "Quiz 2", "Putting it all together",
         "HTTP and the web", "Datacenter networks", "Content delivery networks (CDNs)",
-        "The scarcity of addresses: NAT and IPv6", "Designing a modern application", "Quiz 3", "TBD",
+        "The scarcity of addresses: NAT and IPv6", "Designing a modern application", "Quiz 3", "Completely optional class",
     ]
     notes = [
         "lec1-intro.pdf", "lec2-architecture.pdf", "lec3-agentic-coding.pdf", "lec3-names.pdf", "lec4-routing-1.pdf",
-        "lec5-routing-2.pdf", "lec6-bgp.pdf", "lec7-bgp-advanced.pptx", "lec8-phy-1.pdf",
+        "lec5-routing-2.pdf", "lec6-bgp.pdf", "lec7-bgp-advanced.pptx", 
         "lec9-te.pdf", "lec9-error-detect.pdf", None,
         "lec12-mac-protocols.pptx", "lec13-transport-intro.pdf", "lec14-transport-adv-reliability.pdf",
         "lec15-congestion-control-1.pdf", "lec16-qd-and-switches.pdf", "lec19-security-crypto.pdf",
